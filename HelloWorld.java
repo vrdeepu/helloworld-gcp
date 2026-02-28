a8c291e5af7a25f4303b5b016d292e1bc2ba6a29
vrdeepu <vr.deepu@yahoo.com>
2026-02-28 16:43:59 +0530

    devops full cycle code

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("----------------------------------------");
+        System.out.println("Hello World from Java!");
+        System.out.println("Running on a GCP VM deployed via Jenkins.");
+        System.out.println("System Time: " + new java.util.Date());
+        System.out.println("----------------------------------------");
+    }
+}
