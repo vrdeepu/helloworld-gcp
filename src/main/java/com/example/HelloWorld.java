09c4dd8586888219b38076e01e5d6b37eeba7ff8
vrdeepu <vr.deepu@yahoo.com>
2026-03-14 00:48:07 +0530

    for helloworld webpage

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -1,9 +1,24 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("----------------------------------------");
-        System.out.println("Hello World from Java!");
-        System.out.println("Running on a GCP VM deployed via Jenkins.");
-        System.out.println("System Time: " + new java.util.Date());
-        System.out.println("----------------------------------------");
+package com.example;
+
+import com.sun.net.httpserver.HttpServer;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class HelloWorld { // Matches your filename exactly
+    public static void main(String[] args) throws Exception {
+        // Bind to port 8080
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/", (exchange) -> {
+            String response = "<h1>Success!</h1><p>Java Web Server is live on GCP VM.</p><p>Deployed via Jenkins Pipeline.</p>";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+
+        System.out.println("Web Server started on port 8080...");
+        server.setExecutor(null);
+        server.start();
     }
 }
